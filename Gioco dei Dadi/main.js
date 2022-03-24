a5d854de275884c7b4b82f4b7784a3e0580815d5
Alec <alemor98@hotmail.it>
2022-03-24 14:36:21 +0100

    Prove per la generazione casuale dei numeri

---
+++ Gioco dei Dadi/main.js
@@ -0,0 +1,12 @@
+let numeroPC = Math.random();
+console.log(numeroPC);
+
+numeroPC *= 6;
+console.log(numeroPC);
+
+numeroPC = Math.floor(numeroPC);
+console.log(numeroPC);
+
+numeroPC += 1;
+console.log(numeroPC); 
+
