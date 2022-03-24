aa916b46ccb3e24d68d14c3b5c0618550021f77a
Alec <alemor98@hotmail.it>
2022-03-24 14:57:51 +0100

    Spaziature

--- Gioco dei Dadi/main.js
+++ Gioco dei Dadi/main.js
@@ -1,12 +1,12 @@
 let numeroPC = Math.floor( ( Math.random() * 6 ) + 1 );
 
-console.log("Il numero del computer è: "+numeroPC);
+console.log("Il numero del computer è: " + numeroPC);
 
 let numeroG1 = Math.floor( ( Math.random() * 6 ) + 1 );
 
-console.log("Il tuo numero è: "+numeroG1);
+console.log("Il tuo numero è: " + numeroG1);
 
-if(numeroG1>numeroPC) {
+if(numeroG1 >= numeroPC) {
     console.log("Hai vinto!");
 }else {
     console.log("Hai perso, ha vinto il Computer!");
